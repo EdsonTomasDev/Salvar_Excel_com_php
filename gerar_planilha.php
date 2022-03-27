39b0cf95fd7db846ec410fe92683fd6b0fcbd896
Tomas Edson <t.edson.almeida@hotmail.com>
2022-03-26 21:18:54 -0300

    criado sql de consulta e exportação para excel

--- gerar_planilha.php
+++ gerar_planilha.php
@@ -15,49 +15,75 @@
 
     $link = $banco->conecta_mysql();
 
+    $sql = 'SELECT * FROM funcionarios_grat';
+
+    $consulta = mysqli_query($link,$sql);
+
+  
+
 
 
     //DEFINE O NOME DO ARQUIVO
-    $arquivo = 'msgcontatos.xls';
+    $arquivo = 'aval_gcm.xls';
 
     //CRIAMOS UMA TABELA HTML COM O FORMATO DA PLANILHA
     $html = '';
     $html.= '<table border="1">';
     $html.= '<tr>';
-    $html.= '<td colspan="5">Planilha Mensagens de Contatos</td>';
+    $html.= '<td colspan="16">Valores da GCM após avaliação</td>';
     $html.= '</tr>';
 
     $html.= '<tr>';
-    $html.= '<td> ID </td>';
-    $html.= '<td> Nome </td>';
-    $html.= '<td> E-mail </td>';
-    $html.= '<td> Assunto </td>';
-    $html.= '<td> Data </td>';
+    $html.= '<td> Matrícula </td>';
+    $html.= '<td> Funcionário </td>';
+    $html.= '<td> Função </td>';
+    $html.= '<td> Organograma </td>';
+    $html.= '<td> Valor_Grat </td>';
+    $html.= '<td> Período_aval </td>';
+    $html.= '<td> CPF_chefia </td>';
+    $html.= '<td> Nome_chefia </td>';
+    $html.= '<td> Atrasos </td>';
+    $html.= '<td> Faltas </td>';
+    $html.= '<td> Cooperacao </td>';
+    $html.= '<td> Eficiência </td>';
+    $html.= '<td> Total_pontos </td>';
+    $html.= '<td> porcentagem </td>';
+    $html.= '<td> grat_final </td>';
+    $html.= '<td> Status </td>';
     $html.= '</tr>';
 
-    $html.= '<tr>';
-    $html.= '<td> 1 </td>';
-    $html.= '<td> Tomas Edson </td>';
-    $html.= '<td> tomas@gmail.com </td>';
-    $html.= '<td> Novo cadastro criado </td>';
-    $html.= '<td> 26/03/2022 </td>';
-    $html.= '</tr>';
+    if($consulta){
+        while($resultado_consulta = mysqli_fetch_assoc($consulta)){
 
-    $html.= '<tr>';
-    $html.= '<td> 2 </td>';
-    $html.= '<td> Gabriel da Silva </td>';
-    $html.= '<td> gabriel@gmail.com </td>';
-    $html.= '<td> Novo cadastro Gabriel </td>';
-    $html.= '<td> 20/03/2022 </td>';
-    $html.= '</tr>';
+            $html.= '<tr>';
+            $html.= '<td>'.$resultado_consulta["matricula"].'</td>';
+            $html.= '<td>'.$resultado_consulta["nome_func"].'</td>';
+            $html.= '<td>'.$resultado_consulta["funcao_func"].'</td>';
+            $html.= '<td>'.$resultado_consulta["organograma"].'</td>';
+            $html.= '<td>'.$resultado_consulta["valor_grat"].'</td>';
+            $html.= '<td>'.$resultado_consulta["periodo_aval"].'</td>';
+            $html.= '<td>'.$resultado_consulta["cpf_chefia"].'</td>';
+            $html.= '<td>'.$resultado_consulta["nome_chefia"].'</td>';
+            $html.= '<td>'.$resultado_consulta["atrasos"].'</td>';
+            $html.= '<td>'.$resultado_consulta["falta"].'</td>';
+            $html.= '<td>'.$resultado_consulta["cooperacao"].'</td>';
+            $html.= '<td>'.$resultado_consulta["eficiencia"].'</td>';
+            $html.= '<td>'.$resultado_consulta["total_pontos"].'</td>';
+            $html.= '<td>'.$resultado_consulta["porcentagem"].'</td>';
+            $html.= '<td>'.$resultado_consulta["grat_final"].'</td>';
+            $html.= '<td>'.$resultado_consulta["status"].'</td>';
+    
+            $html.= '</tr>';
 
-    $html.= '<tr>';
-    $html.= '<td> 3 </td>';
-    $html.= '<td> Joelma Ribeiro </td>';
-    $html.= '<td> joelma@gmail.com </td>';
-    $html.= '<td> Novo cadastro Joelma </td>';
-    $html.= '<td> 29/02/2022 </td>';
-    $html.= '</tr>';
+        }
+
+      
+    }else{
+        echo 'Erro ao tentar se conectar ao banco de dados!';
+        die();
+    }
+
+ 
 
     $html.= '</table>';
    
